d93d0b442e8ee9df91feeec55a4e8ddffd57249b
SlavikGusarov <slavik.gusarv@gmail.com>
2017-10-21 15:07:30 +0300

    Start project with maket and initial spring

---
+++ src/main/java/com/gsn/GSN/GsnApplication.java
@@ -0,0 +1,12 @@
+package com.gsn.GSN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsnApplication.class, args);
+	}
+}
