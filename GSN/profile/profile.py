36121cb5ded170e5c8e71154f985068a3bc36f7d
Tatjana <filishtinskaja@gmail.com>
2017-11-26 21:34:33 +0200

    current_app.config

--- GSN/profile/profile.py
+++ GSN/profile/profile.py
@@ -3,7 +3,7 @@ import smtplib
 import random
 import string
 from flask_mail import Mail, Message
-from flask import Flask, render_template, request, g, redirect, session, url_for,json,jsonify, Blueprint
+from flask import Flask, render_template, request, g, redirect, session, url_for,json,jsonify, Blueprint,current_app
 from flask_mysqldb import MySQL
 from werkzeug import check_password_hash, generate_password_hash
 import random
@@ -15,7 +15,6 @@ from GSN import database
 from ..config import photos
 
 mod = Blueprint('profile', __name__, template_folder='templates')
-UPLOADED_PHOTOS_DEST = 'static/img/user'
 
 @mod.route("/myprofile", methods = ['POST'])
 def myprofpost():
@@ -48,7 +47,7 @@ def myprofget():
     user = database.Users.query.filter_by(user_id=g.user.user_id).first()
     user_info = database.UsersInfo.query.filter_by(user_id=g.user.user_id).first()
     if user_info.ava_ref is not None:
-        ref_ava = UPLOADED_PHOTOS_DEST + '/' + user_info.ava_ref
+        ref_ava = current_app.config['UPLOADED_PHOTOS_DEST'] + '/' + user_info.ava_ref
     else:
         ref_ava = 'static/img/avatar.png'
     return render_template('MyProfileSettings.html', ava=ref_ava, name=user.name, surname=user_info.surname, email=user.email, country=user_info.country, city=user_info.city,date=user_info.date,sex=user_info.sex,telephone=user_info.telephone, about=user_info.about)
@@ -59,13 +58,13 @@ def upload():
     if 'photo' in request.files:
         user_info = database.UsersInfo.query.filter_by(user_id=g.user.user_id).first()
         if user_info.ava_ref is not None:
-            os.remove(UPLOADED_PHOTOS_DEST + '/' + user_info.ava_ref)
+            os.remove(current_app.config['UPLOADED_PHOTOS_DEST'] + '/' + user_info.ava_ref)
 
         fname = photos.save(request.files['photo'])
 
         #appending random prefix to name of the file to prevent name collision
         rand_prefix = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(15))
-        os.rename(UPLOADED_PHOTOS_DEST + '/' + fname, UPLOADED_PHOTOS_DEST + '/' + rand_prefix + fname) 
+        os.rename(current_app.config['UPLOADED_PHOTOS_DEST'] + '/' + fname, current_app.config['UPLOADED_PHOTOS_DEST'] + '/' + rand_prefix + fname) 
         user_info.ava_ref = rand_prefix + fname
         database.db.session.commit()
     return redirect(url_for('profile.myprofget'))
