612254df7cf6a7ad0f4bbf7e085c74c4a1da0099
Tatjana <filishtinskaja@gmail.com>
2017-12-13 14:58:11 +0200

    messages+styles

--- GSN/message/message.py
+++ GSN/message/message.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, g, redirect, session, url_for, json, jsonify, Blueprint
 # Database
 from GSN import database
-# Messages
-#from .. import socketio
 
 mod = Blueprint('message', __name__, template_folder='templates')
 
@@ -59,48 +57,74 @@ def get_messages(chat_id):
 			# Add information to array
 			members.append([second_member.user_id, second_member.name, second_member.surname])
 			# Create useful variable to save author's fullname
-			author_fullname = second_member.name + second_member.surname
+			# If user has not name or surname, save login
+			if second_member.name == None or second_member.surname == None:
+				second_member_main_info = database.Users.query.filter_by(user_id = message.author_id).first()
+				author_fullname = second_member_main_info.login
+			else: 
+				author_fullname = second_member.name + second_member.surname
 		# if message's author is our user we save his fullname in variable
 		elif message.author_id == members[0][0]:
-			author_fullname = members[0][1] + members[0][2]
+			author_fullname = members[0][1] + " " + members[0][2]
 		# if message's author is our second member we save his fullname in variable
 		else:
-			author_fullname = members[1][1] + members[1][2]
-		# Append jsonrespond
-		jsonrespond.append({'author_fullname': author_fullname, 'text': message.text, 'time': message.date, 'massage_id' : message.message_id})
+			author_fullname = members[1][1] + " " + members[1][2]
 
-	print(jsonrespond)
+		author=database.UsersInfo.query.filter(database.UsersInfo.user_id==message.author_id).first()
+		# Append jsonrespond
+		jsonrespond.append({'ava':author.ava_ref, 'author_fullname': author_fullname, 'text': message.text, 'time': message.date, 'message_id' : message.message_id, 'author_id' : message.author_id, 'current_user' : g.user.user_id })
+
 
 	return json.dumps(jsonrespond)
 
 
 @mod.route("/send/", methods = ["POST"])
 def send_message():
-	text = request.form.get("text")
+	text = request.form.get("msg")
 	chat_id = request.form.get("chat_id")
+
 	# TODO : nice definition of chat id
-	massage = database.Messages(author_id = g.user.user_id, chat_id = chat_id, text = text, read = False)
-	database.db.session.add(massage)
+	chat = database.Chat.query.filter_by(chat_id = chat_id).first()
+
+	#new_chat = database.Chat(first_member_id = g.user.user_id, second_member_id= |id человека на которого подписывается юзер|)
+	#database.db.session.add(new_chat)
+	#database.db.session.commit()
+	#new_message = database.Messages(author_id = |id человека на которого подписывается юзер|, chat_id = new_chat.chat_id, text = "Привет. Давай общаться!", read = False)
+	#database.db.session.add(new_message)
+	#database.db.session.commit()
+	#new_chat.last_message_id = new_message.message_id
+	#database.db.session.commit()
+
+	message = database.Messages(author_id = g.user.user_id, chat_id = chat_id, text = text, read = False, date = cur_time())
+	database.db.session.add(message)
+	database.db.session.commit()
+
+	chat.last_message_id = message.message_id
+	database.db.session.commit()
+
+	jsonrespond =[]
+	# добавить фото и время
+	jsonrespond.append({'name': g.user_info.name + ' ' + g.user_info.surname, 'text': text})
+	return json.dumps(jsonrespond)
+
+
+
+
+
+
+
+
+@mod.route("/delete/<int:message_id>", methods = ["GET"])
+def delete_message():
+	print(message_id)
+	# Find message
+	message = database.Messages.query.filter_by(message_id = message_id).first()
+	db.session.delete(message)
 	database.db.session.commit()
 
 
-@mod.route("/edit", methods = ["POST"])
-def edit_message():
-	# TODO
-	pass
+def cur_time():
+    now = datetime.datetime.now()
+    return ( (('0'+str(now.hour)) if (now.hour<10) else str(now.hour))+':'+(('0'+str(now.minute)) if (now.minute<10) else str(now.minute))+' '+str(now.day)+'/'+str(now.month))
 
-@mod.route("/delete", methods = ["POST"])
-def delete_message():
-	# TODO
-	pass
 
-@mod.route("/delete_story", methods = ["POST"])
-def delete_story():
-	# TODO
-	pass	
-#
-#
-#
-#
-#
-#
